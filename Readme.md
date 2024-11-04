a9dbb4f55f9d96aaa1ecf486366af36df6281b3a
etnolog11 <volkovvladyslav.off@gmail.com>
2024-11-04 19:16:27 +0100

    Updated Readme, started rewriting everything from scratch starting with the UI part. It now can show a window that is divided in three sections and has a proper name

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 The TKinter project in Python was first started to learn the basics of Python and featured simple GUI, 
 Dark Theme and the ability to play against a simple bot. I then revisited it and implemented the minimax algorithm and 
-tried to clean up the structure with the new acquired knowledge from Java.
+tried to clean up the structure with the new acquired knowledge from Java. And then decided to rewrite it from scratch,
+so now it is being refactored.
---
+++ main_frame.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("TicTacToe")
+    root.resizable(False, False)
+    frame_height = 600
+    frame_width = frame_height * 2
+    # Set the size of the window and center it
+    root.geometry(
+        f"{frame_width}x{frame_height}+{int((root.winfo_screenwidth() - frame_width) / 2)}+{int((root.winfo_screenheight() - frame_height) / 2)}")
+    root.update()
+    normal_colors = {"background": "#000000", "letters": "#FFFFFF", "buttons": "#FFFFFF", "field": "#FFFFFF"}
+    field_canvas = Canvas(root, bg=normal_colors["background"], height=frame_height, width=int(frame_width / 2))
+    field_canvas.place(x=0, y=0)
+    specific_ui_canvas = Canvas(root, bg=normal_colors["background"], height=int(frame_height * 0.75),
+                                width=int(frame_width / 2))
+    specific_ui_canvas = specific_ui_canvas.place(x=int(frame_width / 2 + 1), y=0)
+    general_ui_canvas = Canvas(root, bg=normal_colors["background"], height=int(frame_height / 4),
+                               width=int(frame_width / 2))
+    general_ui_canvas.place(x=int(frame_width / 2 + 1), y=int(frame_height * 0.75 + 1))
+    root.mainloop()
